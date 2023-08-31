fabdf1aba5402ff222fa18daf0fbeb5f1ba0f0a9
uvens <uventus1992@gmail.com>
2023-08-31 17:28:56 +0300

    fixed app_logging.py

--- app_logger.py
+++ app_logger.py
@@ -6,7 +6,7 @@ _dateftm: str = "%d/%B/%Y %H:%M:%S"
 
 
 def get_file_handler(name: str) -> logging.FileHandler:
-    log_dir_name: str = "/home/uventus/PycharmProjects/Morservice/logging"
+    log_dir_name: str = "XL_IDP_PATH_MORSERVICE_SCRIPTS/logging"
     if not os.path.exists(log_dir_name):
         os.mkdir(log_dir_name)
     file_handler: logging.FileHandler = logging.FileHandler(f"{log_dir_name}/{name}.log")
